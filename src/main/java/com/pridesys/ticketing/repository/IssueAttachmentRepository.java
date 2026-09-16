30439a27070aa94cb7372cb9f6c86caa18dae07a
alif666 <aliflodi@gmail.com>
2026-09-17 02:21:11 +0600

    fix: paginate attachment listings

--- src/main/java/com/pridesys/ticketing/repository/IssueAttachmentRepository.java
+++ src/main/java/com/pridesys/ticketing/repository/IssueAttachmentRepository.java
@@ -1,3 +1,3 @@
 package com.pridesys.ticketing.repository;
 import com.pridesys.ticketing.entity.*; import java.util.*; import org.springframework.data.jpa.repository.JpaRepository;
-public interface IssueAttachmentRepository extends JpaRepository<IssueAttachmentEntity,Long> { List<IssueAttachmentEntity> findByIssueIdOrderByCreatedAtAsc(long issueId); }
+public interface IssueAttachmentRepository extends JpaRepository<IssueAttachmentEntity,Long> { org.springframework.data.domain.Page<IssueAttachmentEntity> findByIssueIdOrderByCreatedAtAsc(long issueId,org.springframework.data.domain.Pageable pageable); }
